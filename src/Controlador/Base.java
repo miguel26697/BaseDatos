9289db99b61ff5b36209fcf08294ea13741a5162
migue <miguelrippe01@gmail.com>
2020-05-24 20:24:21 -0500

    hola

--- src/Controlador/Base.java
+++ src/Controlador/Base.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package Controlador;
+
 import Vista.VistaBase;
 import Vista.VistaPropietarios;
 import Vista.VistaRegistroP;
@@ -15,11 +16,13 @@ import Vista.VistaZonas;
 import Vista.VistaTraRes;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 /**
  *
  * @author Jairo
  */
-public class Base  implements ActionListener {
+public class Base implements ActionListener {
+
     private VistaBase vistabase;
     private VistaPropietarios vistapro;
     private VistaRegistroP vistarp;
@@ -29,63 +32,63 @@ public class Base  implements ActionListener {
     private VistaRegistoTrab vistart;
     private VistaZonas vistazona;
     private VistaTraRes vistatr;
-    
-    public Base(VistaBase vistabase,VistaPropietarios vistapro,VistaRegistroP vistarp, VistaResS vistars,VistaRegistroResid vistarr,VistaTrab vistatrab,VistaRegistoTrab vistart,VistaZonas vistazona,VistaTraRes vistatr){
-    this.vistabase=vistabase;
-    this.vistapro=vistapro;
-    this.vistarp=vistarp;
-    this.vistars=vistars;
-    this.vistarr=vistarr;
-    this.vistatrab=vistatrab;
-    this.vistart=vistart;
-    this.vistazona=vistazona;
-    this.vistatr=vistatr;
-    this.vistabase.propietario.addActionListener(this);
-    this.vistabase.registropro.addActionListener(this);
-    this.vistabase.registroresi.addActionListener(this);
-    this.vistabase.registrotrab.addActionListener(this);
-    this.vistabase.residentes.addActionListener(this);
-    this.vistabase.resitrab.addActionListener(this);
-    this.vistabase.trabajadores.addActionListener(this);
-    this.vistabase.zonas.addActionListener(this);
+
+    public Base(VistaBase vistabase, VistaPropietarios vistapro, VistaRegistroP vistarp, VistaResS vistars, VistaRegistroResid vistarr, VistaTrab vistatrab, VistaRegistoTrab vistart, VistaZonas vistazona, VistaTraRes vistatr) {
+        this.vistabase = vistabase;
+        this.vistapro = vistapro;
+        this.vistarp = vistarp;
+        this.vistars = vistars;
+        this.vistarr = vistarr;
+        this.vistatrab = vistatrab;
+        this.vistart = vistart;
+        this.vistazona = vistazona;
+        this.vistatr = vistatr;
+        this.vistabase.propietario.addActionListener(this);
+        this.vistabase.registropro.addActionListener(this);
+        this.vistabase.registroresi.addActionListener(this);
+        this.vistabase.registrotrab.addActionListener(this);
+        this.vistabase.residentes.addActionListener(this);
+        this.vistabase.resitrab.addActionListener(this);
+        this.vistabase.trabajadores.addActionListener(this);
+        this.vistabase.zonas.addActionListener(this);
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent x) {
-        if(x.getSource()== vistabase.propietario){
-          
-         vistabase.setVisible(false);
-        vistapro.setVisible(true);
+        if (x.getSource() == vistabase.propietario) {
+
+            vistabase.setVisible(false);
+            vistapro.setVisible(true);
         }
-        if(x.getSource()==vistabase.registropro){
-        vistabase.setVisible(false);
-        vistarp.setVisible(true);
+        if (x.getSource() == vistabase.registropro) {
+            vistabase.setVisible(false);
+            vistarp.setVisible(true);
         }
-        if(x.getSource()==vistabase.residentes){
-        vistabase.setVisible(false);
-        vistars.setVisible(true);
+        if (x.getSource() == vistabase.residentes) {
+            vistabase.setVisible(false);
+            vistars.setVisible(true);
         }
-        if(x.getSource()==vistabase.registroresi){
-        vistabase.setVisible(false);
-        vistarr.setVisible(true);
+        if (x.getSource() == vistabase.registroresi) {
+            vistabase.setVisible(false);
+            vistarr.setVisible(true);
         }
-        if(x.getSource()==vistabase.trabajadores){
-        vistabase.setVisible(false);
-        vistatrab.setVisible(true);
+        if (x.getSource() == vistabase.trabajadores) {
+            vistabase.setVisible(false);
+            vistatrab.setVisible(true);
         }
-        if(x.getSource()==vistabase.registrotrab){
-        vistabase.setVisible(false);
-        vistart.setVisible(true);
+        if (x.getSource() == vistabase.registrotrab) {
+            vistabase.setVisible(false);
+            vistart.setVisible(true);
         }
-        if(x.getSource()== vistabase.resitrab){
+        if (x.getSource() == vistabase.resitrab) {
             vistabase.setVisible(false);
             vistatr.setVisible(true);
         }
-         if(x.getSource()== vistabase.zonas){
+        if (x.getSource() == vistabase.zonas) {
             vistabase.setVisible(false);
             vistazona.setVisible(true);
         }
-        
+
     }
-    
+
 }
